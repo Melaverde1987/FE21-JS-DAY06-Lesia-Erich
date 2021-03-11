91fd83e58e931a48d034a4281b7b87c0e8f64448
Erich Kiss <erich.kiss2@gmail.com>
2021-03-11 13:31:08 +0100

    toggle size

--- challenge_tesla_erich/js/script.js
+++ challenge_tesla_erich/js/script.js
@@ -2,11 +2,11 @@ let cars = JSON.parse(carsDB);
 console.log(cars);
 
 for (let i of cars) {
-    document.getElementById('viewCards').innerHTML += `<div class="col-lg-4 col-md-6">
+    document.getElementById('viewCards').innerHTML += `<div class="col-lg-4 col-md-6 ${i.Id}">
                                                             <div class="card">
                                                             <img src="${i.image}" class="card-img-top" alt="...">
                                                             <div class="card-body">
-                                                                <h4 id="car_name" class="card-title">${i.brand}</h4>
+                                                                <h4 id="${i.Id}" class="card-title">${i.brand}</h4>
                                                                 <h6 class="card-title">${i.type}</h6>
                                                                 <p class="card-text">${i.text}</p>
                                                                 <p class="card-text">KM: ${i.km} to price of ${i.price} €</p>
@@ -15,14 +15,26 @@ for (let i of cars) {
                                                         </div>
                                                     </div>`
 
-                                                  
+                                           
 
+}
+
+function enlargen (x) {
+    console.log("Large " + x);
+    // this.style.opacity = "0";
+    document.getElementsByClassName(x)[0].classList.toggle('col-lg-4'); 
+    document.getElementsByClassName(x)[0].classList.toggle('col-md-6'); 
+    document.getElementsByClassName(x)[0].classList.toggle('col-lg-12');
+    document.getElementsByClassName(x)[0].classList.toggle('col-md-12');
+};
+
+for (let i of cars) {
+    document.getElementById(i.Id).addEventListener("click", function(){enlargen(i.Id)});
+    console.log(i.Id);
 }
 
 // document.getElementById("car_name").addEventListener("click", enlargen);  
 
-// function enlargen () {
-//     console.log("Large");
-// };
+
 
 
